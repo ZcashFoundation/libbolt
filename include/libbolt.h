6763bdf5ab1ef97e784b9f9864d6a52ecce33e0a
J. Ayo Akinyele <ayo@boltlabs.io>
2019-03-09 23:05:30 -0500

    ffi interface for C

---
+++ include/libbolt.h
@@ -0,0 +1,9 @@
+#ifndef LIBBOLT_INCLUDE_H_
+#define LIBBOLT_INCLUDE_H_
+
+#include <stdint.h>
+
+extern "C" {
+   // Add Bolt methods here for open, closing and dispute txs
+}
+#endif // LIBBOLT_INCLUDE_H_
