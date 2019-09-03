927b14aa7d21ff52f46dc0a1e2f14f6b8582500a
J. Ayo Akinyele <ayo@boltlabs.io>
2019-09-03 16:58:05 +0200

    Fix cflags/ldflags for go code

--- go/libbolt.go
+++ go/libbolt.go
@@ -1,7 +1,7 @@
 package _go
 
-// #cgo darwin CFLAGS: -D LIBSOLV_INTERNAL -I /Library/Developer/CommandLineTools/usr/include/c++/v1 -I ../include -D LD_LIBRARY_PATH=./target/release/
-// #cgo darwin LDFLAGS: -L ./target/release/ -llibbolt
+// #cgo darwin CFLAGS: -I ../include -D LD_LIBRARY_PATH=../target/release 
+// #cgo darwin LDFLAGS: -L ../target/release/ -lbolt
 // #include <libbolt.h>
 import "C"
 
